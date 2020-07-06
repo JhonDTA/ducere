f8090c82eb69600e9d5d226940e2e4c0d4f2ccaf
Jhonatan Tovar <jhondta@icloud.com>
2020-07-05 23:06:54 -0500

    Adds navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
-  devise_for :users
+
+  devise_for :user
 end
