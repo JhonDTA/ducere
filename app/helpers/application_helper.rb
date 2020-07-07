e5f741004c4b97a9ef545c4b4f762817eaebd578
Jhonatan Tovar <jhondta@icloud.com>
2020-07-07 16:50:20 -0500

    Fixes alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@
 
 # Application Helper
 module ApplicationHelper
+
+  # @param [String] type
+  # @return [String]
+  #
+  def flash_type(type)
+    case type
+    when 'notice'
+      'success'
+    when 'alert'
+      'danger'
+    else
+      type
+    end
+  end
 end
