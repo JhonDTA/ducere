c4f187d14c720476080838b66075f3ac9213daef
Jhonatan Tovar <jhondta@icloud.com>
2020-07-08 00:16:00 -0500

    Adds translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,13 @@ module ApplicationHelper
       type
     end
   end
+
+  # Returns the full title on a per-page basis.
+  # @param [String] page_title
+  # @return [String]
+  #
+  def full_title(page_title = '')
+    base_title = t('app_name')
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
