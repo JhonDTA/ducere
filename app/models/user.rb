c0a640380ab9041d3d245adbe5324351219317a1
Jhonatan Tovar <jhondta@icloud.com>
2020-07-25 01:56:00 -0500

    Adds user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
+
+  has_one_attached :avatar
+  validates :avatar, content_type: { in: %w[image/jpeg image/gif image/png],
+                                     message: 'must be a valid format' },
+            size: { less_than: 5.megabytes,
+                    message: 'should be less than 5 megabytes' }
 end
