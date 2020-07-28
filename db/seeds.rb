f130381c17a8bea4dae6dd8d24029e4c934976de
Jhonatan Tovar <jhondta@icloud.com>
2020-07-28 02:45:24 -0500

    Creates default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,28 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# Character.create(name: 'Luke', movie: movies.first)
+
+def create_users
+  User.create(first_name: 'John', last_name: 'Doe', second_last_name: 'Toe',
+              email: 'foo@bar.com', password: 'password',
+              password_confirmation: 'password', confirmed_at: Time.zone.now)
+end
+
+def create_countries
+  require 'csv'
+  CSV.foreach('db/external_data/countries.csv', headers: true) do |row|
+    Country.create(name: row['nombre'], iso_code: row['iso3'],
+                   calling_code: row['phone_code'])
+  end
+end
+
+def create_statuses
+  Status.create(code: 'ACT', name: 'Activo', description: 'Estatus activo general')
+  Status.create(code: 'INA', name: 'Inactivo', description: 'Estatus inactivo general')
+end
+
+create_users
+create_countries
+create_statuses
